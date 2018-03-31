3d0f8e4be602d3deeda7a4573f67d32d17824702
Jorge Valdeiglesias <12958852+argjv@users.noreply.github.com>
2018-03-31 15:34:00 -0700

    Clean up the properties table

--- src/user/ui/propertiesTable/PropertiesTable.js
+++ src/user/ui/propertiesTable/PropertiesTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Griddle from 'griddle-react';
+import Griddle, {RowDefinition, ColumnDefinition} from 'griddle-react';
 
 class PropertiesTable extends Component {
   componentDidMount() {
@@ -9,7 +9,18 @@ class PropertiesTable extends Component {
   render() {
     return (
       <div className="pure-u-1-1 reduced-font">
-        <Griddle data={this.props.propertiesData} />
+        <Griddle data={this.props.propertiesData} >
+          <RowDefinition>
+            <ColumnDefinition id="address1" title="Address" order={1} width={180}/>
+            <ColumnDefinition id="address2" title="Apt/Suite" width={80}/>
+            <ColumnDefinition id="city" title="City" width={80}/>
+            <ColumnDefinition id="estate" title="State" width={80}/>
+            <ColumnDefinition id="beds" title="Beds" width={80}/>
+            <ColumnDefinition id="baths" title="Baths" width={80}/>
+            <ColumnDefinition id="sqft" title="Sqft" width={80}/>
+            <ColumnDefinition id="price" title="Price" width={80}/>
+          </RowDefinition>
+        </Griddle>
       </div>
     );
   }
