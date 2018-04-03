4b7c418b82ed998a9204840bd9e8e8ffbbd1b91a
Jorge Valdeiglesias <12958852+argjv@users.noreply.github.com>
2018-04-02 22:48:00 -0700

    Add a button to the properties table to publish it

--- src/user/ui/propertiesTable/PropertiesTable.js
+++ src/user/ui/propertiesTable/PropertiesTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Griddle, {RowDefinition, ColumnDefinition} from 'griddle-react';
+import { Button, ButtonToolbar } from 'reactstrap';
 
 class PropertiesTable extends Component {
   componentDidMount() {
@@ -7,6 +8,12 @@ class PropertiesTable extends Component {
   }
 
   render() {
+    const Toolbar = ({ value }) => (
+      <ButtonToolbar>
+        <Button color="primary" name="Publish">
+          Publish
+        </Button>
+      </ButtonToolbar>);
     return (
       <div className="pure-u-1-1 reduced-font">
         <Griddle data={this.props.propertiesData} >
@@ -19,6 +26,7 @@ class PropertiesTable extends Component {
             <ColumnDefinition id="baths" title="Baths" width={80}/>
             <ColumnDefinition id="sqft" title="Sqft" width={80}/>
             <ColumnDefinition id="price" title="Price" width={80}/>
+            <ColumnDefinition id="_id" title=" " customComponent={Toolbar} />
           </RowDefinition>
         </Griddle>
       </div>
